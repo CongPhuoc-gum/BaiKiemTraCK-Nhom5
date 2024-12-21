d30f9e23b7516f2df7e3a3b0713bfecfb6e5ec83
Vansinh26904 <Vansinhmb3@gmail.com>
2024-12-21 08:14:29 +0700

    upd bottom_nav

--- app/src/main/java/com/example/baikiemtrack/MainActivity.java
+++ app/src/main/java/com/example/baikiemtrack/MainActivity.java
@@ -16,5 +16,64 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Khởi tạo ViewPager và BottomNavigationView
+        viewPager = findViewById(R.id.view_pager);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Thiết lập adapter cho ViewPager
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+
+        // Thiết lập listener cho ViewPager
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // Reset tất cả các mục
+                bottomNavigationView.getMenu().findItem(R.id.menu_home).setChecked(false);
+                bottomNavigationView.getMenu().findItem(R.id.menu_profile).setChecked(false);
+
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_home).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_profile).setChecked(true);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.menu_home) {
+                    viewPager.setCurrentItem(0);
+                } else if (item.getItemId() == R.id.menu_profile) {
+                    viewPager.setCurrentItem(1);
+                }
+                return true;
+            }
+        });
+    }
 }
